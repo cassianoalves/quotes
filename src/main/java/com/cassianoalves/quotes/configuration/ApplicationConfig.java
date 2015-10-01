72ade0144ef89e02fb3370d5526c61f644a42506
Cassiano Alves <cassiano.alves@gmail.com>
2015-10-01 20:01:11 -0300

    Ajustes configurações da aplicação e de persistência

--- src/main/java/com/cassianoalves/quotes/configuration/ApplicationConfig.java
+++ src/main/java/com/cassianoalves/quotes/configuration/ApplicationConfig.java
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@ComponentScan({"com.cassianoalves.quotes.*"})
-//@EnableAutoConfiguration
+@Configuration
 public class ApplicationConfig {
 }
